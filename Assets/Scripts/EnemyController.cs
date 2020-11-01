32904ed565908cc6a67ecfb2e9b2ca91b86f2e12
Tom Cahill <me@tomcahill.co.uk>
2020-11-01 21:46:04 +0000

    ADDED: Sprite Animation

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -12,10 +12,13 @@ public class EnemyController : MonoBehaviour
     public float timer;
     int direction = 1;
 
+    Animator animator;
+
     // Start is called before the first frame update
     void Start() {
         rigidbody2d = GetComponent<Rigidbody2D>();
         timer = changeTime;
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -26,6 +29,15 @@ public class EnemyController : MonoBehaviour
             direction = -direction;
             timer = changeTime;
         }
+
+        if (vertical) {
+            animator.SetFloat("Move X", 0);
+            animator.SetFloat("Move Y", direction);
+        } else {
+            animator.SetFloat("Move X", direction);
+            animator.SetFloat("Move Y", 0);
+        }
+
     }
 
     private void FixedUpdate() {
--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -19,10 +19,13 @@ public class RubyController : MonoBehaviour
     float horizontal;
     float vertical;
 
+    Animator animator;
+    Vector2 lookDirection = new Vector2(1,0);
 
     // Start is called before the first frame update
     void Start() {
         rigidbody2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
 
         currentHealth = maxHealth;
     }
@@ -31,6 +34,16 @@ public class RubyController : MonoBehaviour
     void Update() {
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
+
+        Vector2 move = new Vector2(horizontal, vertical);
+        if(!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f)) {
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
+        }
+
+        animator.SetFloat("Look X", lookDirection.x);
+        animator.SetFloat("Look Y", lookDirection.y);
+        animator.SetFloat("Speed", move.magnitude);
         
         if (isInvincible) {
             invincibleTimer -= Time.deltaTime;
@@ -49,6 +62,8 @@ public class RubyController : MonoBehaviour
     public void ChangeHealth(int amount) {
         if (amount < 0) {
             if (isInvincible) return;
+
+            animator.SetTrigger("Hit");
 
             isInvincible = true;
             invincibleTimer = timeInvincible;
